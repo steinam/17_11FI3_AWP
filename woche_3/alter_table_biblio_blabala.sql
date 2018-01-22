f9591e3ce990b9a97cf352e3d3c261454047ff30
Karl Steinam <webmaster@steinam.de>
2018-01-22 13:05:46 +0100

    Übung zu alter table hinzugefügt

---
+++ woche_3/alter_table_biblio_blabala.sql
@@ -0,0 +1,13 @@
+﻿alter table autor
+	add primary key(AutorID);
+
+alter table autor
+	modify A_Name char(20),
+    modify AutorID integer not null  auto_increment;
+
+
+
+alter table buch
+	add column VerlagID integer,
+    add foreign key(VerlagID) references `verlag`(VerlagID);
+
